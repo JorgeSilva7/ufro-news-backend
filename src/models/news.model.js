5de17b1dbe3790d544ac7cdffb36d100fdfed8fe
Jorge Silva <14844897+JorgeSilva7@users.noreply.github.com>
2023-10-05 01:24:17 -0300

    timestamp

--- src/models/news.model.js
+++ src/models/news.model.js
@@ -1,30 +1,33 @@
 import mongoose from "mongoose";
 
-const newsSchema = new mongoose.Schema({
-	newsId: {
-		type: String,
-		required: true,
-		unique: true,
-		index: true,
+const newsSchema = new mongoose.Schema(
+	{
+		newsId: {
+			type: String,
+			required: true,
+			unique: true,
+			index: true,
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		imageUrl: {
+			type: String,
+			required: true,
+		},
+		source: {
+			type: String,
+			required: true,
+		},
+		details: {
+			images: [String],
+			body: String,
+			resume: String,
+		},
 	},
-	title: {
-		type: String,
-		required: true,
-	},
-	imageUrl: {
-		type: String,
-		required: true,
-	},
-	source: {
-		type: String,
-		required: true,
-	},
-	details: {
-		images: [String],
-		body: String,
-		resume: String,
-	},
-});
+	{ timestamps: true }
+);
 
 const newsModel = mongoose.model("News", newsSchema);
 
